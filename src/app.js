39e968727318bc2fd4e20ea519e679a935e17d48
SY PHUNG TIEN <ptsy@fresher.local>
2019-09-06 16:38:24 +0700

    init-ver2

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import validator from 'validator';
+
+const template = <h1>This is JSX</h1>
+const place = document.getElementById("root");
+ReactDOM.render(template,place)
